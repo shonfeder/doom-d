d4f9b002700ce35a2411e2579a5503a1df78fba3
Shon Feder <shon@caredox.com>
2019-05-25 07:09:25 -0400

    Initial commit

---
+++ config.el
@@ -0,0 +1,33 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+
+;;;; Code:
+(setq-default evil-escape-key-sequence "jk")
+(setq-default evil-escape-unordered-key-sequence 'true)
+
+;;; ELIXIR
+
+
+;; Create a buffer-local hook to run elixir-format on save, only when we enable elixir-mode.
+(add-hook 'elixir-mode-hook
+          (lambda () (add-hook 'before-save-hook 'lsp-format-buffer nil t)))
+
+(def-package! elixir-mode
+  :after (lsp-mode))
+
+(def-package! lsp-mode
+  :hook
+  (elixir-mode . lsp)
+  :init
+  (add-to-list 'exec-path "/Users/shonfeder/opt/elixir-ls-release"))
+
+;; TODO
+;; (with-eval-after-load 'elixir-mode
+;;   (spacemacs/declare-prefix-for-mode 'elixir-mode
+;;                                      "mt" "tests" "testing related functionality")
+;;   (spacemacs/set-leader-keys-for-major-mode 'elixir-mode
+;;                                             "tb" 'exunit-verify-all
+;;                                             "ta" 'exunit-verify
+;;                                             "tk" 'exunit-rerun
+;;                                             "tt" 'exunit-verify-single))
