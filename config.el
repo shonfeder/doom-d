9b79a79da2e968bc27b3464e987bb1dcb67d6457
Shon Feder <shon.feder@gmail.com>
2019-09-15 01:11:56 -0400

    Remove elixir config functions

--- config.el
+++ config.el
@@ -23,34 +23,6 @@
 (add-hook 'text-mode-hook 'flyspell-mode)
 (add-hook 'prog-mode-hook 'flyspell-prog-mode)
 
-;;;; ELIXIR
-
-(require 'lsp-mode)
-
-(setq flycheck-elixir-mix-executable "/Users/shonfeder/.asdf/shims/mix")
-(setq elixir-enable-compilation-checking t)
-
-(add-hook! elixir-mode
-  (lsp)
-  (setq lsp-ui-flycheck-enable t))
-
-;; TODO elixir-mix checker is not working for some reason failing with
-;; Suspicious state from syntax checker elixir-mix: Flycheck checker elixir-mix returned non-zero exit code 1, but its output contained no errors: ** (Mix) The task "elixir" could not be found
-;; (require 'flycheck-mix)
-;; (flycheck-mix-setup)
-;; (flycheck-add-next-checker 'elixir-mix 'elixir-credo)
-;; (flycheck-add-next-checker 'elixir-mix '(warning . elixir-credo))
-
-;; TODO
-;; (with-eval-after-load 'elixir-mode
-;;   (spacemacs/declare-prefix-for-mode 'elixir-mode
-;;                                      "mt" "tests" "testing related functionality")
-;;   (spacemacs/set-leader-keys-for-major-mode 'elixir-mode
-;;                                             "tb" 'exunit-verify-all
-;;                                             "ta" 'exunit-verify
-;;                                             "tk" 'exunit-rerun
-;;                                             "tt" 'exunit-verify-single))
-
 ;;;; ORG
 
 (add-hook! org-tree-slide-mode
