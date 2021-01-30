4502dd50b421994dbcfdee214b2332c0f9a2cd34
Shon Feder <shon.feder@gmail.com>
2021-01-30 09:41:27 -0500

    Support for emoji and pyright in python

--- init.el
+++ init.el
@@ -30,7 +30,7 @@
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       ;;(emoji +unicode)  ; 🙂
+       (emoji +unicode)  ; 🙂
        fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
@@ -166,7 +166,8 @@
        ;;purescript        ; javascript, but functional
        (python
         +lsp
-        +pyright)            ; beautiful is better than ugly
+        +pyright
+        )            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
